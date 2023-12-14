a292b5c50328363d4dd6df45fdba2ed403da1866
Biasiolo <biasiolorafael@gmail.com>
2023-12-13 21:09:57 -0300

    fix menu

--- menuFullScreen.js
+++ menuFullScreen.js
@@ -1,31 +1,34 @@
 // fullScreenMenu Plugin
-(function($) {
-    $.fn.fullScreenMenu = function(options) {
+(function ($) {
+    $.fn.fullScreenMenu = function (options) {
         // Default options
         var settings = $.extend({
             menuTrigger: '.nav-trigger',
             closeButton: '.js_close_button',
-            menuElement: '.nav-menu',
+            menuElement: '.menu-active',
+            containerElement: '.container',
             animationDuration: 200
         }, options);
 
         // Plugin logic
-        return this.each(function() {
+        return this.each(function () {
             var $this = $(this);
 
-            $this.on('click', settings.menuTrigger + ', ' + settings.darkTrigger, function () {
+            $this.on('click', settings.menuTrigger, function () {
                 $(this).toggleClass('on');
                 $(settings.menuElement).fadeToggle(settings.animationDuration);
                 $(settings.menuElement).toggleClass('active');
+                $(settings.containerElement).toggleClass('menu-open');
             });
 
             $this.on('click', settings.closeButton, function () {
-                $(settings.menuTrigger + ', ' + settings.darkTrigger).removeClass('on');
+                $(settings.menuTrigger).removeClass('on');
                 $(settings.menuElement).removeClass('active');
+                $(settings.containerElement).removeClass('menu-open');
             });
         });
     };
-}(jQuery));
+})(jQuery);
 
 // Usage
 $(document).ready(function () {
@@ -34,6 +37,7 @@ $(document).ready(function () {
         menuTrigger: '.nav-trigger',
         closeButton: '.js_close_button',
         menuElement: '.nav-menu',
+        containerElement: '.container',
         animationDuration: 200
     });
 });
