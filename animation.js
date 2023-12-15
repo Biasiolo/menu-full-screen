ce3d27662a1481b7223345d9bccc7338a98e8ef7
Biasiolo <biasiolorafael@gmail.com>
2023-12-14 23:30:17 -0300

    fix bubbles

--- animation.js
+++ animation.js
@@ -3,9 +3,7 @@ $(document).ready(function () {
     createAntDots();
 
     // Animação constante dos pontos
-    setInterval(function () {
-        animateAntDots();
-    }, 3000); // Tempo de intervalo entre animações (3 segundos)
+    animateAntDots();
 });
 
 function createAntDots() {
@@ -13,43 +11,41 @@ function createAntDots() {
     var dotsContainer = $("<div>").addClass("dots-container").appendTo("body");
 
     // Número de pontos
-    var numberOfDots = 200;
+    var numberOfDots = 100;
 
     for (var i = 0; i < numberOfDots; i++) {
         var dot = $("<div>").addClass("dot").html('<i class="fas fa-ant"></i>').appendTo(dotsContainer);
+
+        // Inicie cada ponto com posição e opacidade aleatórias
+        dot.css({
+            center: Math.random() * window.innerWidth,
+            top: Math.random() * (window.innerHeight * 0.9),
+            opacity: Math.random(),
+        });
     }
 }
 
 function animateAntDots() {
-    var centerX = window.innerWidth / 1;
-    var centerY = window.innerHeight / 1;
-    var allowedAreaX = window.innerWidth * 1;
-    var allowedAreaY = window.innerHeight * 0.5;
+    var allowedAreaX = window.innerWidth;
+    var allowedAreaY = window.innerHeight * 0.9;
 
     $(".dot").each(function (index) {
         var dot = $(this);
 
-        // Animação de movimento dos pontos para o centro
+        // Animação de movimento aleatório
+        var randomX = Math.random() * allowedAreaX;
+        var randomY = Math.random() * allowedAreaY;
+
         dot.animate(
             {
-                center: centerX,
-                top: centerY,
-                opacity: 0.2,
+                left: randomX,
+                top: randomY,
+                opacity: Math.random(),
             },
-            Math.random() * 4000 + 1000, // Tempo de animação entre 1 e 4 segundos
+            Math.random() * 10000 + 2000, // Tempo de animação entre 1 e 4 segundos
             function () {
-                // Após alcançar o centro, espalhe novamente
-                var randomX = Math.random() * allowedAreaX;
-                var randomY = Math.random() * allowedAreaY;
-
-                dot.animate(
-                    {
-                        left: randomX,
-                        top: randomY,
-                        opacity: 1,
-                    },
-                    Math.random() * 4000 + 1000 // Tempo de animação entre 1 e 4 segundos
-                );
+                // Chama a animação novamente
+                animateAntDots();
             }
         );
     });
